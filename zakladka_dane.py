10f640994d71f031b2d3a5785bc667d7d5e92af7
davmis <dawid.miskowicz27@gmail.com>
2026-04-19 22:35:24 +0200

    zmienione dane

--- zakladka_dane.py
+++ zakladka_dane.py
@@ -4,10 +4,12 @@ class Dane:
     def wyswietl_zakladke_dane(self):
         st.header("Dane źródłowe")
         st.write("Tutaj moesz przejeć dane, które zostały wykorzystane w tym projekcie: ")
+        st.markdown("Wszystkie dane wykorzystane w tym projekcie pochodzą z zasobów **Głównego Urzędu Statystycznego**.")
+        st.info("**Oficjalne źródło:** [Bank Danych Lokalnych (BDL)](https://bdl.stat.gov.pl/bdl/dane/podgrup/temat)")
         pliki = {
-            "Wskazniki cen mieszkań (GUS)": "wskazniki_czyste.csv",
-            "średnie zarobki (GUS)": "zarobki_czyste.csv",
-            "Pełne dane GUS (surowe)": "dane_gus.csv"
+            "Wskazniki inflacji": "wskazniki_czyste.csv",
+            "średnie zarobki": "zarobki_czyste.csv",
+            "Ceny za m2": "dane_gus.csv"
         }
         for nazwa, sciezka in pliki.items():
             with st.expander(f"Tabela: {nazwa}"):
